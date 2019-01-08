8e7622a21b1ff6cf1b85d2970889be9c300295ea
Abdullah Selek <abdullahselek@gmail.com>
2019-01-08 22:42:03 +0000

    Created init.

---
+++ facereg/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Face recognition from identity cards with OpenCV and Deep Learning."""
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2019 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/verifid/facereg'
+__download_url__ = 'https://github.com/verifid/facereg'
+__description__  = 'Face recognition from identity cards with OpenCV and Deep Learning.'
