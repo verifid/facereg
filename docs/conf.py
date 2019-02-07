72c4f84d66a5337c164eb09e0a12559c84c349cc
Abdullah Selek <abdullahselek@gmail.com>
2019-02-07 22:21:08 +0000

    Bump version.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ author = 'Abdullah Selek'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
