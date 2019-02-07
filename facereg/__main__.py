0fe37058548ac72cb0c63729b155d180939deea5
Abdullah Selek <abdullahselek@gmail.com>
2019-02-07 18:55:11 +0000

    Argument parser for image downloader.

--- facereg/__main__.py
+++ facereg/__main__.py
@@ -9,3 +9,25 @@ from facereg import (
     face_encoder,
     recognize_faces
 )
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='CLI - face recognition from identity cards with OpenCV and Deep Learning.')
+    parser.add_argument('-d', '--download', type=str, help='keyword to download images from Google')
+    parser.add_argument('-l', '--limit', type=int, default=3, help='number of image is going to be downloaded from Google, max 20')
+    args = parser.parse_args()
+
+    if len(sys.argv) < 2:
+        print('Specify a key to use')
+        sys.exit(1)
+
+    # Optional bash tab completion support
+    try:
+        import argcomplete
+        argcomplete.autocomplete(parser)
+    except ImportError:
+        pass
+
+    args = parser.parse_args()
+    if args.download != None:
+        google_images.download(args.download, limit=args.limit)
