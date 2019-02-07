a68fd32ba84fc771e16e92e6fe6730a024797ac4
Abdullah Selek <abdullahselek@gmail.com>
2019-02-07 22:08:28 +0000

    Add argument for face recognition.

--- facereg/__main__.py
+++ facereg/__main__.py
@@ -6,7 +6,6 @@ import argparse
 
 from facereg import (
     google_images,
-    face_encoder,
     recognize_faces
 )
 
@@ -15,6 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CLI - face recognition from identity cards with OpenCV and Deep Learning.')
     parser.add_argument('-d', '--download', type=str, help='Keyword to download images from Google')
     parser.add_argument('-l', '--limit', type=int, default=3, help='Number of image is going to be downloaded from Google, max 20')
+    parser.add_argument('-i', '--image', type=str, help='Directory path of your image for recognition')
     args = parser.parse_args()
 
     if len(sys.argv) < 2:
@@ -31,3 +31,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.download != None:
         google_images.download(args.download, limit=args.limit)
+    if args.image != None:
+        print(recognize_faces.recognize(args.image))
