705e124aef916773cf65f59355ae07277a3c0f19
Abdullah Selek <abdullahselek@gmail.com>
2019-06-10 14:47:47 +0100

    Proper fix on updated download function.

--- facereg/google_images.py
+++ facereg/google_images.py
@@ -19,12 +19,12 @@ def download(keywords, type='face', limit=20, output_directory=output_directory)
       output_directory (str):
         Directory name in which the images are downloaded.
     Returns:
-      paths (dictionary):
+      paths (dictionary), errors (str):
         Image paths that has downloaded images.
     """
 
     downloader = google_images_download.googleimagesdownload()
     arguments = {'keywords': keywords, 'limit': limit, 
                  'output_directory': output_directory, 'print_urls': True}
-    paths = downloader.download(arguments)
-    return paths
+    paths, errors = downloader.download(arguments)
+    return paths, errors
