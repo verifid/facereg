0cf3029fa8b312e56105901a6a3e96f6f872417d
Abdullah Selek <abdullahselek@gmail.com>
2019-07-30 08:41:11 +0100

    Check image path if available.

--- facereg/face_encoder.py
+++ facereg/face_encoder.py
@@ -28,19 +28,20 @@ def encode_faces(datasets=datasets_path,
     known_names = []
     image_paths = list(paths.list_images(datasets))
 
-    for (i, imagePath) in enumerate(image_paths):
+    for (i, image_path) in enumerate(image_paths):
         print('face_encoder:encode_faces processing image {}/{}'.format(i + 1, len(image_paths)))
-        name = imagePath.split(os.path.sep)[-2]
-        image = cv2.imread(imagePath)
-        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # detect the (x, y) coordinates of the bounding boxes
-        boxes = face_recognition.face_locations(rgb, model=detection_method)
-        # compute the facial embedding for the face
-        face_encodings = face_recognition.face_encodings(rgb, boxes)
-        # loop over the encodings
-        for face_encode in face_encodings:
-            known_encodings.append(face_encode)
-            known_names.append(name)
+        name = image_path.split(os.path.sep)[-2]
+        if os.path.isfile(image_path):
+            image = cv2.imread(image_path)
+            rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # detect the (x, y) coordinates of the bounding boxes
+            boxes = face_recognition.face_locations(rgb, model=detection_method)
+            # compute the facial embedding for the face
+            face_encodings = face_recognition.face_encodings(rgb, boxes)
+            # loop over the encodings
+            for face_encode in face_encodings:
+                known_encodings.append(face_encode)
+                known_names.append(name)
     # save facial encodings and names to disk
     print('face_encoder:encode_faces serializing encodings...')
     data = {"encodings": known_encodings, "names": known_names}
